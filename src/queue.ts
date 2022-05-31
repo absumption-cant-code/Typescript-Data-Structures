f37e9b2e8e8df194db45496d0f4da30d22936fa4
absumption-cant-code <michaelloconte0405@gmail.com>
2022-05-31 19:55:56 -0400

    Queue documentation

--- src/queue.ts
+++ src/queue.ts
@@ -8,11 +8,37 @@
 export class queue<T> {
     private array: T[] = [];
 
+    /**
+     * Return the length of the array.
+     * @returns The length of the array.
+     */
     public size(): number { return this.array.length };
+
+    /**
+     * Return true if the array is empty, otherwise return false.
+     * @returns The length of the array.
+     */
     public isEmpty(): boolean { return this.array.length === 0 };
+
+    /**
+     * Return the first element of the array.
+     * @returns The first element in the array.
+     */
     public peek(): T { return this.array[0] };
 
+    /**
+     * "This function adds an element to the end of the queue."
+     * 
+     * The function is public because it needs to be called outside of the class
+     * @param {T} data - T - The data that we want to add to the queue.
+     */
     public enqueue(data: T): void { this.array.push(data) };
+
+    /**
+     * If the array is empty, return undefined. If the array has at least one element, return the first
+     * element of the array
+     * @returns The first element in the array.
+     */
     public dequeue(): T | undefined { 
         if (this.array.length === 0) { return undefined };
         if (this.array.length >= 1) { return this.array.shift() };
